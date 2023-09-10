49fd700c8ad34371c9eec3f6c4dab1086c037ca5
liujd2 <liujiandong0326@163.com>
2023-09-10 21:47:54 +0800

    feat: 集成 auto import & router & vueUse

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,10 +1,20 @@
 <template>
   <div>
-    index
+    {{ msg }}
     <h1 class="border-double border-4 border-indigo-600 w-2xl">哈哈</h1>
+    <div ref="target" class="border-4">
+      <h1>Hello world</h1>
+      <p>x: {{ x }}</p>
+      <p>y: {{ y }}</p>
+      <p>isOutside: {{ isOutside }}</p>
+    </div>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const msg = ref('Hello World1')
+const target = ref(null)
+const { x, y, isOutside } = useMouseInElement(target)
+</script>
 
 <style scoped></style>
--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
 import VueRouter from 'unplugin-vue-router/vite'
+import AutoImport from 'unplugin-auto-import/vite'
+import { VueRouterAutoImports } from 'unplugin-vue-router'
 
 import UnoCSS from 'unocss/vite'
 
@@ -16,7 +18,23 @@ export default defineConfig({
     }),
     vue(),
     vueJsx(),
-    UnoCSS()
+    UnoCSS(),
+    AutoImport({
+      include: [
+        /\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
+        /\.vue$/,
+        /\.vue\?vue/, // .vue
+        /\.md$/ // .md
+      ],
+
+      // global imports to register
+      imports: [
+        // presets
+        'vue',
+        VueRouterAutoImports,
+        '@vueuse/core'
+      ]
+    })
   ],
   resolve: {
     alias: {
