24718e5d5eb95707aee6321287ca0a241ef89f84
liujd2 <liujiandong0326@163.com>
2023-09-11 21:23:24 +0800

    feat: 集成 unocss 里的 icon 方案

--- uno.config.ts
+++ uno.config.ts
@@ -1,6 +1,13 @@
-import { defineConfig } from 'unocss'
-import presetWind from '@unocss/preset-wind'
+import { defineConfig, presetWind, presetIcons } from 'unocss'
 
 export default defineConfig({
-  presets: [presetWind()]
+  presets: [
+    presetWind(),
+    presetIcons({
+      prefix: 'i-',
+      extraProperties: {
+        display: 'inline-block' // 默认会占满一行，所以需要设置为 inline-block
+      }
+    })
+  ]
 })
