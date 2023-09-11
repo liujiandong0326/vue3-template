a49ac189fa14f99594ba0a4aca1aeb61eb3937a6
liujd2 <liujiandong0326@163.com>
2023-09-11 21:10:25 +0800

    feat: 在 unplugin-vue-components 集成中使用 element plus

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -10,6 +10,14 @@
     </div>
     <HelloWorld />
     <UserUserComponent />
+    <el-row class="mt-4">
+      <el-button>Default</el-button>
+      <el-button type="primary">Primary</el-button>
+      <el-button type="success">Success</el-button>
+      <el-button type="info">Info</el-button>
+      <el-button type="warning">Warning</el-button>
+      <el-button type="danger">Danger</el-button>
+    </el-row>
   </div>
 </template>
 
--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ import VueRouter from 'unplugin-vue-router/vite' // 文件系统路由
 import AutoImport from 'unplugin-auto-import/vite' // 自动引入 vue vue-router 语法糖
 import { VueRouterAutoImports } from 'unplugin-vue-router'
 import Components from 'unplugin-vue-components/vite' // 自动引入自定义组件
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 import UnoCSS from 'unocss/vite'
 
@@ -37,7 +38,8 @@ export default defineConfig({
       ]
     }),
     Components({
-      directoryAsNamespace: true // 允许子目录作为组件的命名空间前缀，就是使用组件的时候也得加上目录名称
+      directoryAsNamespace: true, // 允许子目录作为组件的命名空间前缀，就是使用组件的时候也得加上目录名称
+      resolvers: [ElementPlusResolver()]
     })
   ],
   resolve: {
