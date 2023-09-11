8b8b0978036b84e7f983e189d53a33b274baa663
liujd2 <liujiandong0326@163.com>
2023-09-11 21:56:39 +0800

    feat: 集成 vite-plugin-vue-layouts

---
+++ src/layouts/home.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <div>header</div>
+    <router-view />
+    <div>footer</div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,8 @@ import VueRouter from 'unplugin-vue-router/vite' // 文件系统路由
 import AutoImport from 'unplugin-auto-import/vite' // 自动引入 vue vue-router 语法糖
 import { VueRouterAutoImports } from 'unplugin-vue-router'
 import Components from 'unplugin-vue-components/vite' // 自动引入自定义组件
-import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers' // 配置自动引入 element plus
+import Layouts from 'vite-plugin-vue-layouts'
 
 import UnoCSS from 'unocss/vite'
 
@@ -40,6 +41,10 @@ export default defineConfig({
     Components({
       directoryAsNamespace: true, // 允许子目录作为组件的命名空间前缀，就是使用组件的时候也得加上目录名称
       resolvers: [ElementPlusResolver()]
+    }),
+    Layouts({
+      layoutsDirs: 'src/layouts',
+      defaultLayout: 'default'
     })
   ],
   resolve: {
